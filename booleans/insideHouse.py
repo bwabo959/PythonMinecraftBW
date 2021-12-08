05d7295894109e47e3432546ace0a60b9f8c4a7c
Bryan Wabo <bwabo959@lsr7.net>
2021-12-08 13:47:44 -0600

    added insideHouse.py

--- booleans/insideHouse.py
+++ booleans/insideHouse.py
@@ -1,15 +1,16 @@
-from mcpi. minecraft import minecraft
-mc = minecraft.create()
- buildX =
- buildY =
- buildZ =
- width = 10
- height = 5
- length = 6
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+buildX = -7.5
+buildY = 0.0
+buildZ = 73.9
+width = 4
+height = 5
+length = 7
  
- pos = mc.player.getTilePos()
- x = pos.x
- y = pos.y
- z = pos.z
+pos = mc.player.getTilePos()
+x = pos.x
+y = pos.y
+z = pos.z
  
- inside = buildX< x< buildX + width and 
+inside = buildX< x< buildX + width and buildY < y < buildY + height and buildZ< z< buildZ + length
+mc.postToChat("The player is at home: " + str(inside))
