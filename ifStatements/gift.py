ee471c0d14c99cd49dca0801546df5fbbca248c5
Bryan Wabo <bwabo959@lsr7.net>
2021-12-09 13:45:51 -0600

    added gift.py

--- ifStatements/gift.py
+++ ifStatements/gift.py
@@ -1,8 +1,13 @@
 from mcpi.minecraft import Minecraft
-mc + Minecraft.create()
-x = 10
-y = 11
-z = 12
+mc = Minecraft.create()
+x = -9.3
+y = 0.0
+z = 99.5
 gift = mc.getBlock(x, y, z)
 
 if gift == 57:
+    mc.postToChat("Thanks for the diamond.")
+elif gift == 6:
+    mc.postToChat("I guess tree sapplings are as good as diamonds...")
+else:
+    mc.postToChat("Bring a gift to the coordinates.")
