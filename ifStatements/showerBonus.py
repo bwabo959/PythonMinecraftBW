051bbb7d69a148c5f0ddec6adfe9c83e57cbd67e
[bwabo959] <[bwabo959@lsr7.net]>
2021-12-15 14:15:06 -0600

    added Bonus of shower.py

---
+++ ifStatements/showerBonus.py
@@ -0,0 +1,26 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+import time
+
+shwrX = 70.0
+shwrY = 0.0
+shwrZ = 70.0
+
+width = 1
+height = 5
+length = 1
+
+pos = mc.player.getTilePos()
+x = pos.x
+y = pos.y
+z = pos.z
+
+if shwrX <= x < shwrX + width and shwrY <= y < shwrY + height and shwrZ <= z < shwrZ + length:
+    mc.setBlocks(shwrX, shwrY + height, shwrZ, shwrX + width, shwrY + height, shwrZ + length, 8)
+    time.sleep(10)
+    mc.setBlocks(shwrX, shwrY + height, shwrZ, shwrX + width, shwrY + height, shwrZ + length, 0)
+
+
+
+        
+
