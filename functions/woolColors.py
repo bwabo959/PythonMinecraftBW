51e8499ec1438e22272717c7ce4bf7d424210f56
[bwabo959] <[bwabo959@lsr7.net]>
2022-01-03 15:02:37 -0600

    added functions folder

--- functions/woolColors.py
+++ functions/woolColors.py
@@ -1,4 +1,17 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-def getWool
+def getWoolState(color):
+    """ Takes a color as a string and returns the wool block state for that color """
+    if color == "pink":
+        blockState = 6
+    elif color == "black":
+        blockState = 15
+    elif color == "white":
+        blockState = 0
+        
+colorString = input("Enter a block color: ")
+state = getWoolState(colorString)
+
+pos = mc.player.getTilePos()
+mc.setBlock(pos.x, pos.y, pos., 35, state) 
