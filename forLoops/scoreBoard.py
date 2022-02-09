cceeb6821d0e47641bd1ee0c3e1546c38ac2a392
[bwabo959] <[bwabo959@lsr7.net]>
2022-02-09 14:49:46 -0600

    added scorebaord.py

---
+++ forLoops/scoreBoard.py
@@ -0,0 +1,25 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+import time
+
+name = ""
+scoreboard = {}
+
+while True:
+    name = input("What is your name? ")
+    if name == "exit":
+        break
+    mc.postToChat("Go!")
+
+    time.sleep(10)
+
+    blockHits = mc.events.pollBlockHits()
+
+    blockhitsLength = len(blockHits)
+    mc.postToChat("Your score is " + str(blockhitsLength))
+
+    scoreboard[name] = str(blockhitsLength)
+
+    for name in scoreboard:
+        print(scoreboard)
